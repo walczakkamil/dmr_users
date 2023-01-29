ba10b7a7d4a13e297a3db3e24fb9aa5095d09b2f
Kamil Walczak <waperr@interia.pl>
2023-01-29 20:43:24 +0100

    Extract PL stations

--- script.sh
+++ script.sh
@@ -9,3 +9,6 @@ curl -s -o user.csv https://radioid.net/static/user.csv
 echo "Replacing of diacritics"
 iconv -o user_wo_diacritics.csv -f UTF8 -t US-ASCII//TRANSLIT user.csv
 
+echo "Create PL stattion list"
+head -n 1 user.csv > user_PL_wo_diacritics.csv
+grep -E '[,](HF|3Z|S[NOPRQ])[0-9]([A-Z0-9]{0,3}[A-Z]{1})[,]' user_wo_diacritics.csv >> user_PL_wo_diacritics.csv
