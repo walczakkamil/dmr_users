06ad74ffe14b1820ab3368179b43d72f885f43a9
walczakkamil <waperr@interia.pl>
2023-10-16 20:28:07 +0200

    add GitHub action instead of running cron script on private server

--- script.sh
+++ script.sh
@@ -12,3 +12,6 @@ iconv -f UTF8 -t ASCII//IGNORE//TRANSLIT user.csv >  user_wo_diacritics.csv
 echo "Create PL station list"
 head -n 1 user.csv > user_PL_wo_diacritics.csv
 grep -E '[,](HF|3Z|S[NOPRQ])[0-9]([A-Z0-9]{0,3}[A-Z]{1})[,]' user_wo_diacritics.csv >> user_PL_wo_diacritics.csv
+
+mkdir -p databases
+cp user*.csv databases/
